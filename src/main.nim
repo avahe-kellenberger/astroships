c1968456b854e643bb2817fca8f934a85db2ae21
Chris McGrane <cmcgrane@whitebox.com>
2020-10-25 01:23:56 -0400

    testing explosion palette

--- src/main.nim
+++ src/main.nim
@@ -1,20 +1,38 @@
 import nico
 
 var buttonDown = false
+var frame = 0
 
 proc gameInit() =
-  var pal = loadPaletteFromGPL("palette.gpl")
-  loadSpritesheet(1, "sprites/explosion_sheet.png", 32, 32)
-  setPalette(pal)
+  return
+
+proc explosion() =
+  setSpritesheet(2)
+  palt(14, true)
+  spr(0 + ((frame.float / 5.0).int mod 11), 20, 20)
 
 proc gameUpdate(dt: float32) =
+  frame += 1
   buttonDown = btn(pcA)
 
 proc gameDraw() =
   cls()
-  setSpritesheet(1)
-  spr(3, 20, 20)
 
-nico.init("nimtendo", "ASTROSHIPS")
-nico.createWindow("ASTROSHIPS", 128, 128, 4, false)
+  setColor(1)
+  print("click for explosion!", 10, 10)
+
+  if mousebtn(0):
+    explosion()
+  
+
+nico.init("nico", "ASTROSHIPS")
+nico.createWindow("ASTROSHIPS", 128, 128, 4)
+
+# For now lets just get the explosion palette working
+setPalette(loadPaletteFromGPL("explosion.gpl"))
+
+loadSpritesheet(2, "sprites/explosion_sheet.png", 32, 32)
+fixedSize(true)
+integerScale(true)
+
 nico.run(gameInit, gameUpdate, gameDraw)
