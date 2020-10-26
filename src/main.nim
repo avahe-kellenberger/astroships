cb183642c278f33011d6b1bb2cc8d545eace3c21
Chris McGrane <cmcgrane@whitebox.com>
2020-10-25 22:19:09 -0400

    Palette issue

--- src/main.nim
+++ src/main.nim
@@ -1,15 +1,38 @@
-import nico
+import nico, sdl2/sdl
+
+nico.init("nico", "ASTROSHIPS")
+nico.createWindow("ASTROSHIPS", 128, 128, 4)
+fixedSize(true)
+integerScale(true)
 
 var buttonDown = false
 var frame = 0
 
+## Sprite Aliases
+var sprExplosion = 2;
+var sprShip = 3;
+
+# Load Palettes (pal)
+var explosionPalette = loadPaletteFromGPL("pal/explosion.gpl")
+var shipPalette = loadPaletteFromGPL("pal/ship.gpl")
+
 proc gameInit() =
+  # Load sprites
+  loadSpritesheet(sprExplosion, "sprites/explosion_sheet.png", 32, 32)
+  loadSpritesheet(sprShip, "sprites/ship_sheet.png", 16, 16)
   return
 
-proc explosion() =
-  setSpritesheet(2)
+proc drawExplosion() =
+  setPalette(explosionPalette)
   palt(14, true)
-  spr(0 + ((frame.float / 5.0).int mod 11), 20, 20)
+  setSpritesheet(sprExplosion)
+  spr(0 + ((frame.float / 5).int mod 11), 20, 20)
+
+proc drawShip() =
+  setPalette(shipPalette)
+  palt(9, true)
+  setSpritesheet(sprShip)
+  spr(0 + ((frame.float / 15).int mod 3), 60, 60)
 
 proc gameUpdate(dt: float32) =
   frame += 1
@@ -18,21 +41,10 @@ proc gameUpdate(dt: float32) =
 proc gameDraw() =
   cls()
 
-  setColor(1)
+  setColor(7)
   print("click for explosion!", 10, 10)
-
-  if mousebtn(0):
-    explosion()
   
-
-nico.init("nico", "ASTROSHIPS")
-nico.createWindow("ASTROSHIPS", 128, 128, 4)
-
-# For now lets just get the explosion palette working
-setPalette(loadPaletteFromGPL("explosion.gpl"))
-
-loadSpritesheet(2, "sprites/explosion_sheet.png", 32, 32)
-fixedSize(true)
-integerScale(true)
+  drawExplosion()
+  drawShip()
 
 nico.run(gameInit, gameUpdate, gameDraw)
