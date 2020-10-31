ab49f1f7dfa337ff2d62e0753fc955efbed4b9dc
Chris McGrane <cmcgrane@whitebox.com>
2020-10-31 17:39:28 -0400

    Updated text entity with outline

--- astroships/api/entities/textentity.nim
+++ astroships/api/entities/textentity.nim
@@ -1,4 +1,4 @@
-import nico
+import nico, nico/utils
 import ../entity
 
 type TextAlign* = enum
@@ -12,12 +12,16 @@ type TextEntity* = ref object of Entity
   align*: TextAlign
   scale*: int
   color*: int
+  outline*: bool
+  outlineColor*: int
 
 proc newTextEntity*(
   text: string,
   x, y: float,
   align: TextAlign = taLeft,
-  scale, color: int = 1
+  scale, color: int = 1,
+  outline: bool,
+  outlineColor: int = 1
 ): TextEntity =
   result = TextEntity(
     flags: loRender,
@@ -27,6 +31,8 @@ proc newTextEntity*(
     align: align,
     scale: scale,
     color: color,
+    outline: outline,
+    outlineColor: outlineColor,
     textWidth: textWidth(text)
   )
 
@@ -34,11 +40,18 @@ template getTextWidth*(this: TextEntity): int = this.textWidth
 
 method render*(this: TextEntity) =
   setColor(this.color)
+  setOutlineColor(this.outlineColor)
   case this.align:
   of taLeft:
     print(this.text, this.x, this.y, this.scale)
+    if this.outline:
+      printOutline(this.text, this.x, this.y, this.scale)
   of taCenter:
+    if this.outline:
+      printOutlineC(this.text, this.x, this.y, this.scale)
     printc(this.text, this.x, this.y, this.scale)
   of taRight:
+    if this.outline:
+      printOutlineR(this.text, this.x, this.y, this.scale)
     printr(this.text, this.x, this.y, this.scale)
 
--- astroships/scenes/titlescene/titlelayer.nim
+++ astroships/scenes/titlescene/titlelayer.nim
@@ -10,16 +10,15 @@ type TitleLayer* = ref object of Layer
 proc newTitleLayer*: TitleLayer = 
   result = TitleLayer()
   let textScale: Pint = 16
-  ## We need to figure out how to pass in the current canvas width
-  ## otherwise center aligning on screen is going to be a pain.
-  ## I don't think that the text alignment procedures are relative
-  ## to the actual viewport.
   result.add(
     newTextEntity(
       "ASTROSHIPS",
       WINDOW_WIDTH / 2,
       2f,
       taCenter,
-      textScale
+      textScale,
+      1,
+      false,
+      3
     )
   )
